e195d1586b58029dc79c88d6afa33f8ebd02ebbc
Danilo Taco <etaco@saludsa.com.ec>
2025-05-14 15:11:22 -0500

    Postman Anexos

--- src/main/java/com/nttdata/backend/service/MovimientoService.java
+++ src/main/java/com/nttdata/backend/service/MovimientoService.java
@@ -12,6 +12,8 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -76,7 +78,7 @@ public class MovimientoService {
             monto, saldoAnterior);
 
         movimiento.setId(UUID.randomUUID());
-        movimiento.setFecha(Instant.now());
+        movimiento.setFecha(LocalDate.now().atStartOfDay().atZone(ZoneId.systemDefault()).toInstant());
 
         return movimientoRepository.save(movimiento);
     }
